32c1e54179f25a290c0b4f46b1ea1170bb11dcd9
tumat001 <castromatthieu23@gmail.com>
2023-04-27 08:20:23 +0800

    1.3.9 fix for stage 7 map

--- CYDE_SPECIFIC_ONLY/MapRelated/MapList/Map_World07/Map_World07.gd
+++ CYDE_SPECIFIC_ONLY/MapRelated/MapList/Map_World07/Map_World07.gd
@@ -16,3 +16,6 @@ func _apply_map_specific_changes_to_game_elements(arg_game_elements):
 	]
 	arg_game_elements.game_modifiers_manager.add_game_modi_ids(modis)
 	
+	arg_game_elements.enemy_manager.current_path_to_spawn_pattern = arg_game_elements.enemy_manager.PathToSpawnPattern.SWITCH_PER_SPAWN
+
+
